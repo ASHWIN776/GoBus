95420f96102acb2c63715c8cfd52300173e30c65
Ashwin <ashwinrockon123@gmail.com>
2021-06-01 17:12:49 +0530

    Added Homepage, working on Login

---
+++ index.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bus Ticket Bookings</title>
+    <!-- google fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500&display=swap" rel="stylesheet">
+    <!-- Font-awesome -->
+    <script src="https://kit.fontawesome.com/d8cfbe84b9.js" crossorigin="anonymous"></script>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <!-- CSS -->
+    <?php 
+        require 'assets/styles/styles.php'
+    ?>
+</head>
+<body>
+    <header>
+        <nav>
+            <div>
+                    <a href="#" class="nav-item nav-logo">Logo</a>
+                    <a href="#" class="nav-item">Gallery</a>
+            </div>
+                
+            <ul>
+                <li><a href="#" class="nav-item">Home</a></li>
+                <li><a href="#about" class="nav-item">About</a></li>
+                <li><a href="#contact" class="nav-item">Contact</a></li>
+            </ul>
+            <div>
+                <a href="#" class="login nav-item" data-bs-toggle="modal" data-bs-target="#loginModal"><i class="fas fa-sign-in-alt" style="margin-right: 0.4rem;"></i>Login</a>
+                <a href="#pnr-enquiry" class="pnr nav-item">PNR Enquiry</a>
+            </div>
+        </nav>
+    </header>
+    <?php require 'assets/partials/_loginModal.php'?>
+    <section id="home">
+        <div id="route-search-form">
+            <h1>Find Routes</h1>
+            <form action="">
+                <div>
+                    <label for="source">Source</label><br>
+                    <input type="text" id="source" name="source">
+                </div>
+                <div>
+                    <label for="destination">Destination</label><br>
+                    <input type="text" id="destination" name="destination">
+                </div>
+                <div>
+                    <label for="departure">Date of Departure</label><br>
+                    <input type="date" id="departure" name="departure">
+                </div>
+                <div>
+                    <button type="button" name="search">Search</button>
+                </div>
+            </form>
+        </div>
+    </section>
+    <div id="block">
+        <section id="info-num">
+            <figure>
+                <img src="assets/img/route.svg" alt="Bus Route Icon" width="100px" height="100px">
+                <figcaption>
+                    <span class="num">10000</span>
+                    <span class="icon-name">routes</span>
+                </figcaption>
+            </figure>
+            <figure>
+                <img src="assets/img/bus.svg" alt="Bus Icon" width="100px" height="100px">
+                <figcaption>
+                    <span class="num">20000</span>
+                    <span class="icon-name">bus</span>
+                </figcaption>
+            </figure>
+            <figure>
+                <img src="assets/img/customer.svg" alt="Happy Customer Icon" width="100px" height="100px">
+                <figcaption>
+                    <span class="num">30000</span>
+                    <span class="icon-name">happy customers</span>
+                </figcaption>
+            </figure>
+            <figure>
+                <img src="assets/img/ticket.svg" alt="Instant Ticket Icon" width="100px" height="100px">
+                <figcaption>
+                    <span class="num">20 SEC</span>
+                    <span class="icon-name">Instant Tickets</span>
+                </figcaption>
+            </figure>
+        </section>
+        <section id="pnr-enquiry">
+            <div id="pnr-form">
+                <h1>PNR Enquiry</h1>
+                <form action="">
+                    <div>
+                        <label for="pnr">PNR Number : </label>
+                        <input type="text" name="pnr" id="pnr">
+                    </div>
+                    <div>
+                        <button type="button" name="pnr-search">submit</button>
+                    </div>
+                </form>
+            </div>
+        </section>
+        <section id="about">
+            <div>
+                <h1>About Us</h1>
+                <h4>Wanna know were it all started?</h4>
+                <p>
+                    Lorem ipsum dolor sit amet consecteturadipisicing elit. Perferendis soluta voluptas eaque, numquam veritatis aperiam expedita deleniti, nesciunt cum alias velit. Cupiditate commodi
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusamus cum nisi ea optio unde aliquam quia reprehenderit atque eum tenetur! 
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed placeat debitis corporis voluptates modi quibusdam quidem voluptatibus illum, maiores sequi.
+                </p>
+            </div>
+        </section>
+        <section id="contact">
+            <div id="contact-form">
+                <h1>Contact Us</h1>
+                <form action="">
+                    <div>
+                        <label for="name">Name</label>
+                        <input type="text" name="name" id="name">
+                    </div>
+                    <div>
+                        <label for="email">Email Address</label>
+                        <input type="email" name="email" id="email">
+                    </div>
+                    <div>
+                        <label for="message">Message</label>
+                        <textarea name="message" id="message" cols="30" rows="10"></textarea>
+                    </div>
+                    <div></div>
+                </form>
+            </div>
+        </section>
+        <footer>
+            <p>
+                <i class="far fa-copyright"></i> 2021 | Made with &#10084;&#65039; by Ashwin Anil
+            </p>
+        </footer>
+    </div>
+     <!-- Option 1: Bootstrap Bundle with Popper -->
+     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
+</body>
+</html>
