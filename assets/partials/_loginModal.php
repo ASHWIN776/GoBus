dbc6f2ece0da093d189732545772c73aa26ceb33
Ashwin <ashwinrockon123@gmail.com>
2021-06-01 17:26:27 +0530

    Added login Modal

--- assets/partials/_loginModal.php
+++ assets/partials/_loginModal.php
@@ -7,7 +7,7 @@
         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
-        <form action="/forum/layout/_handleLogin.php" method="POST">
+        <form action="assets/partials/_handleLogin.php" method="POST">
           <div class="mb-3">
               <label for="username" class="form-label">Username</label>
               <input type="text" class="form-control" id="username" name="username">
