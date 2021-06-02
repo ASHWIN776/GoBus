664a110c0a9c8560ab8adb78365c38e6fddb9a67
Ashwin <ashwinrockon123@gmail.com>
2021-06-02 16:50:28 +0530

    Checking if the admin is loggedIn before showing the admin pages

--- admin/customer.php
+++ admin/customer.php
@@ -1,3 +1,7 @@
+<!-- Show these admin pages only when the admin is logged in -->
+<?php  require '../assets/partials/_admin-check.php';   ?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,72 +21,9 @@
     ?>
 </head>
 <body>
-    <header>
-        <nav id="navbar">
-            <ul>
-                <li class="nav-item">ADMIN</li>
-                <li class="nav-item">
-                    <img class="adminDp" src="../assets/img/admin_pic.jpg" alt="Admin Profile Pic" width="22px" height="22px">
-                </li>
-            </ul>
-        </nav>
-    </header>
-    <main id="container">
-        <div id="sidebar">
-            <div>
-                <img class="adminDp" src="../assets/img/admin_pic.jpg" alt="Admin Profile Pic">
-                <h3>Admin</h3>
-                <p>Senior Developer</p>
-            </div>
-            <ul id="options">
-                <li class="option">
-                    <a href="./dashboard.html">
-                        Dashboard
-                    </a>
-                </li>
-                <li class="option">
-                    <a href="./booking.html">
-                        Bookings
-                    </a>
-                </li>
-                <li class="option">
-                    <a href="./bus.html">
-                        Buses
-                    </a>
-                </li>
-                <li class="option">
-                    <a href="./route.html">
-                        Routes    
-                    </a>
-                </li>
-                <li class="option">
-                    <a href="./seat.html">
-                        Seats
-                    </a>
-                </li>
-                <li class="option">
-                    <a href="./customer.html">
-                        Customers
-                    </a>
-                </li>
-                <li class="option">
-                    <a href="./signup.html">
-                        Add New Admin        
-                    </a>
-                </li>
-            </ul>
-        </div>
-        <div id="main-content">
-            <section id="welcome">
-                <ul>
-                    <li class="welcome-item">Welcome Admin</li>
-                    <li class="welcome-item">
-                        <button id="logout">
-                            LOGOUT
-                        </button>
-                    </li>
-                </ul>
-            </section>
+    <!-- Requiring the admin header files -->
+    <?php require '../assets/partials/_admin-header.php';?>
+
             <section id="customer">
                 <div id="head">
                     <h2>Customer Status</h2>
--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,3 +1,6 @@
+<!-- Show these admin pages only when the admin is logged in -->
+<?php  require '../assets/partials/_admin-check.php';   ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,72 +20,9 @@
     ?>
 </head>
 <body>
-    <header>
-        <nav id="navbar">
-            <ul>
-                <li class="nav-item">ADMIN</li>
-                <li class="nav-item">
-                    <img class="adminDp" src="../assets/img/admin_pic.jpg" alt="Admin Profile Pic" width="22px" height="22px">
-                </li>
-            </ul>
-        </nav>
-    </header>
-    <main id="container">
-        <div id="sidebar">
-            <div>
-                <img class="adminDp" src="../assets/img/admin_pic.jpg" alt="Admin Profile Pic">
-                <h3>Admin</h3>
-                <p>Senior Developer</p>
-            </div>
-            <ul id="options">
-                <li class="option">
-                    <a href="#">
-                        Dashboard
-                    </a>
-                </li>
-                <li class="option">
-                    <a href="./booking.html">
-                        Bookings
-                    </a>
-                </li>
-                <li class="option">
-                    <a href="./bus.html">
-                        Buses
-                    </a>
-                </li>
-                <li class="option">
-                    <a href="./route.html">
-                        Routes    
-                    </a>
-                </li>
-                <li class="option">
-                    <a href="./seat.html">
-                        Seats
-                    </a>
-                </li>
-                <li class="option">
-                    <a href="./customer.html">
-                        Customers
-                    </a>
-                </li>
-                <li class="option">
-                    <a href="./signup.html">
-                        Add New Admin        
-                    </a>
-                </li>
-            </ul>
-        </div>
-        <div id="main-content">
-            <section id="welcome">
-                <ul>
-                    <li class="welcome-item">Welcome Admin</li>
-                    <li class="welcome-item">
-                        <button id="logout">
-                            <a href="../assets/partials/_logout.php">LOGOUT</a>
-                        </button>
-                    </li>
-                </ul>
-            </section>
+    <!-- Requiring the admin header files -->
+    <?php require '../assets/partials/_admin-header.php';?>
+
             <section id="dashboard">
                 <h3>Status</h3>
                 <div id="status">
@@ -97,7 +37,7 @@
                             <p>Total Bookings</p>
                             <p class="num">24</p>
                         </div>
-                        <a href="./booking.html">View More <i class="fas fa-arrow-right"></i></a>
+                        <a href="./booking.php">View More <i class="fas fa-arrow-right"></i></a>
                     </div>
                     <div id="Bus" class="info-box status-item">
                         <div class="heading">
@@ -110,7 +50,7 @@
                             <p>Total Buses</p>
                             <p class="num">69</p>
                         </div>
-                        <a href="./bus.html">View More <i class="fas fa-arrow-right"></i></a>
+                        <a href="./bus.php">View More <i class="fas fa-arrow-right"></i></a>
                     </div>
                     <div id="Route" class="info-box status-item">
                         <div class="heading">
@@ -123,7 +63,7 @@
                             <p>Total Routes</p>
                             <p class="num">9</p>
                         </div>
-                        <a href="./route.html">View More <i class="fas fa-arrow-right"></i></a>
+                        <a href="./route.php">View More <i class="fas fa-arrow-right"></i></a>
                     </div>
                     <div id="Seat" class="info-box status-item">
                         <div class="heading">
@@ -136,7 +76,7 @@
                             <p>Total Seats</p>
                             <p class="num">50</p>
                         </div>
-                        <a href="./seat.html">View More <i class="fas fa-arrow-right"></i></a>
+                        <a href="./seat.php">View More <i class="fas fa-arrow-right"></i></a>
                     </div>
                 </div>
                 <h3>User</h3>
@@ -152,7 +92,7 @@
                             <p>Total Customers</p>
                             <p class="num">100</p>
                         </div>
-                        <a href="./customer.html">View More <i class="fas fa-arrow-right"></i></a>
+                        <a href="./customer.php">View More <i class="fas fa-arrow-right"></i></a>
                     </div>
                     <div id="Admin" class="info-box user-item">
                         <div class="heading">
