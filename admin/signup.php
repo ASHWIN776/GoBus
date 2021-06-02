1ba1fcb81bb590c3286d2515863bc000d31572b5
Ashwin <ashwinrockon123@gmail.com>
2021-06-02 23:52:53 +0530

    Added: Bootstrap cdn, alert shown after signup

--- admin/signup.php
+++ admin/signup.php
@@ -1,17 +1,18 @@
 <!-- Show these admin pages only when the admin is logged in -->
 <?php  require '../assets/partials/_admin-check.php';   ?>
 
-
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Admin Dashboard</title>
+        <title>Add Admin</title>
             <!-- google fonts -->
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500&display=swap" rel="stylesheet">
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
         <!-- Font Awesome -->
         <script src="https://kit.fontawesome.com/d8cfbe84b9.js" crossorigin="anonymous"></script>
         <!-- External CSS -->
@@ -24,6 +25,28 @@
     <!-- Requiring the admin header files -->
     <?php require '../assets/partials/_admin-header.php';?>
 
+        <!-- Signup Status -->
+            <?php
+                if(isset($_GET['signup']))
+                {
+                    if($_GET['signup'])
+                    {
+                        // Show success alert
+                        echo '<div class="my-0 alert alert-success alert-dismissible fade show" role="alert">
+                        <strong>Successful!</strong> Account created successfully
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>';
+                    }
+
+                    else
+                        // Show error alert
+                        echo '<div class="my-0 alert alert-danger alert-dismissible fade show" role="alert">
+                        <strong>Error!</strong> Username already exists
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                        </div>';
+
+                }
+            ?>
             <section id="add-admin">
                 <div>
                     <div id="signupForm">
@@ -50,6 +73,8 @@
                 <div>
                 </div>
             </section>
-        </div>  
+        </div>
+         <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>  
 </body>
 </html>
