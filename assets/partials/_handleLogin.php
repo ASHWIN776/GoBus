0269be39f93ff829420f133942ac45cd4143e67a
Ashwin <ashwinrockon123@gmail.com>
2021-06-02 14:48:40 +0530

    login creds checked here

---
+++ assets/partials/_handleLogin.php
@@ -0,0 +1,36 @@
+<?php
+    require '_functions.php';
+    $conn = db_connect();
+
+    if(!$conn)
+        die("Oh Shoot!! Connection Failed");
+
+    if($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["submit"]))
+    {
+        echo "All correct";
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+
+        $sql = "SELECT * FROM `users` WHERE user_name='$username';";
+        $result = mysqli_query($conn, $sql);
+
+        if($row = mysqli_fetch_assoc($result)){
+            $hash = $row['user_password'];
+            echo "Got the hash";
+            if(password_verify($password, $hash))
+            {
+                // Login Sucessfull
+                session_start();
+                $_SESSION["loggedIn"] = true;
+                $_SESSION["user_id"] = $row["user_id"];
+
+                header("location: /admin/dashboard.php");
+                exit;
+            }
+            
+            // Login failure
+            $error = true;
+            header("location: index.php?error=$error");
+        }
+    }
+?>
