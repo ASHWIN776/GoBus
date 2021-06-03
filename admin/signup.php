9c7960ffb5f7d1503d341b461d25709a1bb8f7f0
Ashwin <ashwinrockon123@gmail.com>
2021-06-03 19:20:40 +0530

    Modified

--- admin/signup.php
+++ admin/signup.php
@@ -74,7 +74,7 @@
                 </div>
             </section>
         </div>
-         <!-- Option 1: Bootstrap Bundle with Popper -->
+    <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>  
 </body>
 </html>
