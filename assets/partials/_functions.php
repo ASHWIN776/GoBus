1d0491b5f582c36dc5d613015462094e77c1c28c
Ashwin <ashwinrockon123@gmail.com>
2021-06-05 18:55:47 +0530

    added function that checks if a customer exists or not

--- assets/partials/_functions.php
+++ assets/partials/_functions.php
@@ -13,6 +13,7 @@
     function exist_user($conn, $username)
     {
         $sql = "SELECT * FROM `users` WHERE user_name='$username'";
+        
         $result = mysqli_query($conn, $sql);
         $num = mysqli_num_rows($result);
         if($num)
@@ -23,6 +24,18 @@
     function exist_routes($conn, $viaCities, $time)
     {
         $sql = "SELECT * FROM `routes` WHERE route_cities='$viaCities' AND route_timing='$time'";
+
+        $result = mysqli_query($conn, $sql);
+        $num = mysqli_num_rows($result);
+        if($num)
+            return true;
+        return false;
+    }
+
+    function exist_customers($conn, $name, $phone)
+    {
+        $sql = "SELECT * FROM `customers` WHERE customer_name='$name' AND customer_phone='$phone'";
+
         $result = mysqli_query($conn, $sql);
         $num = mysqli_num_rows($result);
         if($num)
