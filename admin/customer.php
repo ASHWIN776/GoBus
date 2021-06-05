cfd88f78c1a170ac27124bcbb1a7e574df447fb6
Ashwin <ashwinrockon123@gmail.com>
2021-06-05 16:40:05 +0530

    Added: Bootstrap links, Add Customer Modal

--- admin/customer.php
+++ admin/customer.php
@@ -14,6 +14,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500&display=swap" rel="stylesheet">
     <!-- Font Awesome -->
     <script src="https://kit.fontawesome.com/d8cfbe84b9.js" crossorigin="anonymous"></script>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <!-- CSS -->
     <?php 
         require '../assets/styles/admin.php';
@@ -26,7 +28,7 @@
 
             <section id="customer">
                 <div id="head">
-                    <h2>Customer Status</h2>
+                    <h4>Customer Status</h4>
                     <div id="search">
                         <div id="wrapper">
                             <input type="text" name="search" placeholder="Search">
@@ -34,10 +36,11 @@
                         </div>
                     </div>
                 </div>
+                <!-- If Customers are present -->
                 <div id="customer-results">
                     <div>
-                        <button id="add-button" class="button">ADD <i class="fas fa-plus"></i></button>
-                    <button id="filter-button" class="button">FILTER <i class="fas fa-filter"></i></button>
+                        <button id="add-button" class="button btn-sm"type="button"data-bs-toggle="modal" data-bs-target="#addModal">ADD <i class="fas fa-plus"></i></button>
+                        <button id="filter-button" class="button btn-sm">FILTER <i class="fas fa-filter"></i></button>
                     </div>
                     <table>
                         <tr>
@@ -123,5 +126,66 @@
             </section>
         </div>
     </main>
+    <!-- All Modals Here -->
+    <!-- Add Route Modal -->
+    <div class="modal fade" id="addModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Add A Customer</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form id="addCustomerForm" action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="POST">
+                            <div class="mb-3">
+                                <label for="c-firstname" class="form-label">Customer Firstname</label>
+                                <input type="text" class="form-control" id="c-firstname" name="c-firstname">
+                            </div>
+                            <div class="mb-3">
+                                <label for="c-lastname" class="form-label">Customer Lastname</label>
+                                <input type="text" class="form-control" id="c-lastname" name="c-lastname">
+                            </div>
+                            <div class="mb-3">
+                                <label for="c-phone" class="form-label">Phone Number</label>
+                                <input type="tel" class="form-control" id="c-phone" name="c-phone">
+                            </div>
+                            <button type="submit" class="btn btn-success" name="submit">Submit</button>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <!-- Add Anything -->
+                    </div>
+                    </div>
+                </div>
+        </div>
+        <!-- Delete Modal -->
+        <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel"><i class="fas fa-exclamation-circle"></i></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <h2 class="text-center pb-4">
+                    Are you sure?
+                </h2>
+                <p>
+                    Do you really want to delete this route id? <strong>This process cannot be undone.</strong>
+                </p>
+                <!-- Needed to pass id -->
+                <form action="<?php echo $_SERVER['REQUEST_URI']; ?>" id="delete-form"  method="POST">
+                    <input id="delete-id" type="hidden" name="id">
+                </form>
+            </div>
+            <div class="modal-footer d-flex justify-content-center">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                <button type="submit" form="delete-form" name="delete" class="btn btn-danger">Delete</button>
+            </div>
+            </div>
+        </div>
+    </div>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 </body>
 </html>
