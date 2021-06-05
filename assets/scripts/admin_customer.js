73d86fca8065308d54fa86d807dc039af27cf68c
Ashwin <ashwinrockon123@gmail.com>
2021-06-05 18:36:42 +0530

    scripts for customer.php, Added: Edit button works

---
+++ assets/scripts/admin_customer.js
@@ -0,0 +1,63 @@
+const resultRows = document.querySelectorAll("tr");
+const editBtns = document.querySelectorAll(".edit-button");
+const deleteBtns = document.querySelectorAll(".delete-button");
+const table = document.querySelector("table");
+
+resultRows.forEach(row => 
+    row.addEventListener("click", editOrDelete)  
+);
+
+if(table)
+{
+    table.addEventListener("click", collapseForm);
+}
+
+function collapseForm(evt){
+    if(evt.target.className.includes("btn-close")){
+        const collapseRow = evt.target.parentElement.parentElement.parentElement.parentElement;
+
+        // enable the edit button
+        const editBtn = collapseRow.previousElementSibling.children[3].children[0];
+        editBtn.disabled = false;
+        editBtn.classList.remove("disabled");
+
+        // Collapse the row
+        collapseRow.remove();
+    }
+}
+
+function editOrDelete(evt){
+    
+    if(evt.target.className.includes("edit-button"))
+    {
+        // Disable the button
+        evt.target.disabled = true;
+        evt.target.classList.add("disabled");
+
+        const editRow = document.createElement("tr");
+        editRow.innerHTML = `
+        <td colspan="5">
+            <form class="editRouteForm d-flex justify-content-between" action="${evt.target.dataset.link}?id=${evt.target.dataset.id}" method="POST">
+                <input type="text" class="form-control" name="cname" value="${evt.target.dataset.name}">
+            
+                <input type="text" class="form-control cphone" name="cphone" value="${evt.target.dataset.phone}">        
+           
+                <div class="d-flex justify-content-between">
+                    <button type="submit" class="btn btn-success btn-sm" name="edit">SUBMIT</button>
+                    <button type="button" class="btn-close align-self-center"></button>
+                </div>
+            </form>
+        </td>
+    `;
+    
+    this.after(editRow);
+    }
+
+    else if(evt.target.className.includes("delete-button"))
+    {
+        const deleteInput = document.querySelector("#delete-id");
+        deleteInput.value = evt.target.dataset.id;
+    }
+}
+
+
