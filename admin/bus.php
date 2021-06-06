acc1bb10b69a40e5cd933a160d547db0f5a197ae
Ashwin <ashwinrockon123@gmail.com>
2021-06-06 17:46:41 +0530

    Add bus functionality will be a yes

--- admin/bus.php
+++ admin/bus.php
@@ -14,6 +14,8 @@
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100;200;300;400;500&display=swap" rel="stylesheet">
     <!-- Font Awesome -->
     <script src="https://kit.fontawesome.com/d8cfbe84b9.js" crossorigin="anonymous"></script>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
     <!-- CSS -->
     <?php 
         require '../assets/styles/admin.php';
@@ -23,9 +25,138 @@
 <body>
     <!-- Requiring the admin header files -->
     <?php require '../assets/partials/_admin-header.php';?>
+
+    <!-- Add, Edit and Delete Buses -->
+    <?php
+        /*
+            1. Check if an admin is logged in
+            2. Check if the request method is POST
+        */
+        if($loggedIn && $_SERVER["REQUEST_METHOD"] == "POST")
+        {
+            if(isset($_POST["submit"]))
+            {
+                /*
+                    ADDING BUSES
+                 Check if the $_POST key 'submit' exists
+                */
+                // Should be validated client-side
+                $busno = $_POST["busnumber"];
+        
+                $bus_exists = exist_buses($conn,$busno);
+                $bus_added = false;
+        
+                if(!$bus_exists)
+                {
+                    // Route is unique, proceed
+                    $sql = "INSERT INTO `buses` (`bus_no`, `bus_created`) VALUES ('$busno', current_timestamp());";
+
+                    $result = mysqli_query($conn, $sql);
+
+                    if($result)
+                        $bus_added = true;
+                }
+    
+                if($bus_added)
+                {
+                    // Show success alert
+                    echo '<div class="my-0 alert alert-success alert-dismissible fade show" role="alert">
+                    <strong>Successful!</strong> Bus Information Added
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>';
+                }
+                else{
+                    
+                    // Show error alert
+                    echo '<div class="my-0 alert alert-danger alert-dismissible fade show" role="alert">
+                    <strong>Error!</strong> Bus already exists
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>';
+                }
+            }
+            if(isset($_POST["edit"]))
+            {
+                // EDIT ROUTES
+                $viaCities = strtoupper($_POST["viaCities"]);
+                $cost = $_POST["stepCost"];
+                $time = $_POST["time"];
+                $id = $_GET["id"];
+
+                $updateSql = "UPDATE `routes` SET
+                `route_cities` = '$viaCities',
+                `route_timing` = '$time', `route_step_cost` = '$cost' WHERE `routes`.`id` = '$id';";
+        
+                $updateResult = mysqli_query($conn, $updateSql);
+                $rowsAffected = mysqli_affected_rows($conn);
+                
+                $messageStatus = "danger";
+                $messageInfo = "";
+                $messageHeading = "Error!";
+
+                if(!$rowsAffected)
+                {
+                    $messageInfo = "No Edits Administered!";
+                }
+
+                elseif($updateResult)
+                {
+                    // Show success alert
+                    $messageStatus = "success";
+                    $messageHeading = "Successfull!";
+                    $messageInfo = "Route details Edited";
+                }
+                else{
+                    // Show error alert
+                    $messageInfo = "Your request could not be processed due to technical Issues from our part. We regret the inconvenience caused";
+                }
+                
+                // MESSAGE
+                echo '<div class="my-0 alert alert-'.$messageStatus.' alert-dismissible fade show" role="alert">
+                <strong>'.$messageHeading.'</strong> '.$messageInfo.'
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>';
+            }
+            if(isset($_POST["delete"]))
+            {
+                // DELETE ROUTES
+                $id = $_POST["id"];
+                // Delete the route with id => id
+                $deleteSql = "DELETE FROM `routes` WHERE `routes`.`id` = $id";
+                $deleteResult = mysqli_query($conn, $deleteSql);
+                $rowsAffected = mysqli_affected_rows($conn);
+                $messageStatus = "danger";
+                $messageInfo = "";
+                $messageHeading = "Error!";
+
+                if(!$rowsAffected)
+                {
+                    $messageInfo = "Record Doesnt Exist";
+                }
+
+                elseif($deleteResult)
+                {   
+                    // echo $num;
+                    // Show success alert
+                    $messageStatus = "success";
+                    $messageInfo = "Route Details deleted";
+                    $messageHeading = "Successfull!";
+                }
+                else{
+                    // Show error alert
+                    $messageInfo = "Your request could not be processed due to technical Issues from our part. We regret the inconvenience caused";
+                }
+                // Message
+                echo '<div class="my-0 alert alert-'.$messageStatus.' alert-dismissible fade show" role="alert">
+                <strong>'.$messageHeading.'</strong> '.$messageInfo.'
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>';
+            }
+        }
+        ?>
+
             <section id="bus">
                 <div id="head">
-                    <h2>Bus Status</h2>
+                    <h4>Bus Status</h4>
                     <div id="search">
                         <div id="wrapper">
                             <a href="#"><i class="fas fa-search"></i></a>
@@ -35,82 +166,17 @@
                 </div>
                 <div id="bus-results">
                     <div>
-                        <button id="add-button" class="button">ADD <i class="fas fa-plus"></i></button>
-                    <button id="filter-button" class="button">FILTER <i class="fas fa-filter"></i></button>
+                        <button id="add-button" class="button btn-sm"type="button"data-bs-toggle="modal" data-bs-target="#addModal">ADD <i class="fas fa-plus"></i></button>
+                        <button id="filter-button" class="button btn-sm">FILTER <i class="fas fa-filter"></i></button>
                     </div>
                     <table>
                         <tr>
                             <th>ID</th>
-                            <th>Route ID</th>
-                            <th>Bus Name</th>
+                            <th>Bus Number</th>
                             <th>Actions</th>
                         </tr>
                         <tr>
                             <td>2002</td>
-                            <td>1729</td>
-                            <td>Pilsburg</td>
-                            <td>
-                                <button class="button edit-button">Edit</button>
-                                <button class="button delete-button">Delete</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>2002</td>
-                            <td>1729</td>
-                            <td>Pilsburg</td>
-                            <td>
-                                <button class="button edit-button">Edit</button>
-                                <button class="button delete-button">Delete</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>2002</td>
-                            <td>1729</td>
-                            <td>Pilsburg</td>
-                            <td>
-                                <button class="button edit-button">Edit</button>
-                                <button class="button delete-button">Delete</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>2002</td>
-                            <td>1729</td>
-                            <td>Pilsburg</td>
-                            <td>
-                                <button class="button edit-button">Edit</button>
-                                <button class="button delete-button">Delete</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>2002</td>
-                            <td>1729</td>
-                            <td>Pilsburg</td>
-                            <td>
-                                <button class="button edit-button">Edit</button>
-                                <button class="button delete-button">Delete</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>2002</td>
-                            <td>1729</td>
-                            <td>Pilsburg</td>
-                            <td>
-                                <button class="button edit-button">Edit</button>
-                                <button class="button delete-button">Delete</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>2002</td>
-                            <td>1729</td>
-                            <td>Pilsburg</td>
-                            <td>
-                                <button class="button edit-button">Edit</button>
-                                <button class="button delete-button">Delete</button>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>2002</td>
-                            <td>1729</td>
                             <td>Pilsburg</td>
                             <td>
                                 <button class="button edit-button">Edit</button>
@@ -122,5 +188,58 @@
             </section>
         </div>
     </main>
+    <!-- All Modals Here -->
+    <!-- Add Buses Modal -->
+    <div class="modal fade" id="addModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Add A Bus</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                        <form id="addCustomerForm" action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="POST">
+                            <div class="mb-3">
+                                <label for="busnumber" class="form-label">Bus Number</label>
+                                <input type="text" class="form-control" id="busnumber" name="busnumber">
+                            </div>
+                            <button type="submit" class="btn btn-success" name="submit">Submit</button>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                        <!-- Add Anything -->
+                    </div>
+                    </div>
+                </div>
+        </div>
+        <!-- Delete Modal -->
+        <div class="modal fade" id="deleteModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel"><i class="fas fa-exclamation-circle"></i></h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <h2 class="text-center pb-4">
+                    Are you sure?
+                </h2>
+                <p>
+                    Do you really want to delete this route id? <strong>This process cannot be undone.</strong>
+                </p>
+                <!-- Needed to pass id -->
+                <form action="<?php echo $_SERVER['REQUEST_URI']; ?>" id="delete-form"  method="POST">
+                    <input id="delete-id" type="hidden" name="id">
+                </form>
+            </div>
+            <div class="modal-footer d-flex justify-content-center">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+                <button type="submit" form="delete-form" name="delete" class="btn btn-danger">Delete</button>
+            </div>
+            </div>
+        </div>
+    </div>
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-gtEjrD/SeCtmISkJkNUaaKMoLD0//ElJ19smozuHV6z3Iehds+3Ulb9Bn9Plx0x4" crossorigin="anonymous"></script>
 </body>
 </html>
