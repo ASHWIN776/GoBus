5ec2830cea80b6f15cf32818f08e94f85c3749f1
Ashwin <ashwinrockon123@gmail.com>
2021-06-12 18:53:13 +0530

    Added: Seats already booked are shown while booking

--- admin/booking.php
+++ admin/booking.php
@@ -98,6 +98,20 @@
                     <strong>Successful!</strong> Booking Added
                     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
                     </div>';
+
+                    // Update the Seats table
+                    $bus_no = get_from_table($conn, "routes", "route_id", $route_id, "bus_no");
+                    $seats = get_from_table($conn, "seats", "bus_no", $bus_no, "seat_booked");
+                    if($seats)
+                    {
+                        $seats .= "," . $booked_seat;
+                    }
+                    else 
+                        $seats = $booked_seat;
+
+                    $updateSeatSql = "UPDATE `seats` SET `seat_booked` = '$seats' WHERE `seats`.`bus_no` = '$bus_no';";
+                    mysqli_query($conn, $updateSeatSql);
+                    echo "Seat Updated";
                 }
                 else{
                     // Show error alert
@@ -444,31 +458,31 @@
                             <!-- Seats Diagram -->
                             <div class="mb-3">
                                 <table id="seatsDiagram">
-                                    <tr>
-                                    <td data-name="1">1</td>
-                                    <td data-name="2">2</td>
-                                    <td data-name="3">3</td>
-                                    <td data-name="4">4</td>
-                                    <td data-name="5">5</td>
-                                    <td data-name="6">6</td>
-                                    <td data-name="7">7</td>
-                                    <td data-name="8">8</td>
-                                    <td data-name="9">9</td>
-                                    <td data-name="10">10</td>
-                                    </tr>
-                                    <tr>
-                                    <td data-name="11">11</td>
-                                    <td data-name="12">12</td>
-                                    <td data-name="13">13</td>
-                                    <td data-name="14">14</td>
-                                    <td data-name="15">15</td>
-                                    <td data-name="16">16</td>
-                                    <td data-name="17">17</td>
-                                    <td data-name="18">18</td>
-                                    <td data-name="19">19</td>
-                                    <td data-name="20">20</td>
-                                    </tr>
-                                    <tr>
+                                <tr>
+                                    <td id="seat-1" data-name="1">1</td>
+                                    <td id="seat-2" data-name="2">2</td>
+                                    <td id="seat-3" data-name="3">3</td>
+                                    <td id="seat-4" data-name="4">4</td>
+                                    <td id="seat-5" data-name="5">5</td>
+                                    <td id="seat-6" data-name="6">6</td>
+                                    <td id="seat-7" data-name="7">7</td>
+                                    <td id="seat-8" data-name="8">8</td>
+                                    <td id="seat-9" data-name="9">9</td>
+                                    <td id="seat-10" data-name="10">10</td>
+                                </tr>
+                                <tr>
+                                    <td id="seat-11" data-name="11">11</td>
+                                    <td id="seat-12" data-name="12">12</td>
+                                    <td id="seat-131" data-name="13">13</td>
+                                    <td id="seat-14" data-name="14">14</td>
+                                    <td id="seat-15" data-name="15">15</td>
+                                    <td id="seat-16" data-name="16">16</td>
+                                    <td id="seat-17" data-name="17">17</td>
+                                    <td id="seat-18" data-name="18">18</td>
+                                    <td id="seat-19" data-name="19">19</td>
+                                    <td id="seat-20" data-name="20">20</td>
+                                </tr>
+                                <tr>
                                         <td class="space">&nbsp;</td>
                                         <td class="space">&nbsp;</td>
                                         <td class="space">&nbsp;</td>
@@ -479,30 +493,30 @@
                                         <td class="space">&nbsp;</td>
                                         <td class="space">&nbsp;</td>
                                         <td class="space">&nbsp;</td>
-                                    </tr>
-                                    <tr>
-                                    <td data-name="21">21</td>
-                                    <td data-name="22">22</td>
-                                    <td data-name="23">23</td>
-                                    <td data-name="24">24</td>
-                                    <td data-name="25">25</td>
-                                    <td data-name="26">26</td>
-                                    <td data-name="27">27</td>
-                                            <td class="space">&nbsp;</td>
-                                    <td data-name="28">28</td>
-                                    <td data-name="29">29</td>
-                                    </tr>
-                                    <tr>
-                                    <td data-name="30">30</td>
-                                    <td data-name="31">31</td>
-                                    <td data-name="32">32</td>
-                                    <td data-name="33">33</td>
-                                    <td data-name="34">34</td>
-                                    <td data-name="35">35</td>
-                                    <td data-name="36">36</td>
+                                </tr>
+                                <tr>
+                                    <td id="seat-21" data-name="21">21</td>
+                                    <td id="seat-22" data-name="22">22</td>
+                                    <td id="seat-23" data-name="23">23</td>
+                                    <td id="seat-24" data-name="24">24</td>
+                                    <td id="seat-25" data-name="25">25</td>
+                                    <td id="seat-26" data-name="26">26</td>
+                                    <td id="seat-27" data-name="27">27</td>
                                     <td class="space">&nbsp;</td>
-                                    <td data-name="37">37</td>
-                                    <td data-name="38">38</td>
+                                    <td id="seat-28" data-name="28">28</td>
+                                    <td id="seat-29" data-name="29">29</td>
+                                </tr>
+                                <tr>
+                                    <td id="seat-30" data-name="30">30</td>
+                                    <td id="seat-31" data-name="31">31</td>
+                                    <td id="seat-32" data-name="32">32</td>
+                                    <td id="seat-33" data-name="33">33</td>
+                                    <td id="seat-34" data-name="34">34</td>
+                                    <td id="seat-35" data-name="35">35</td>
+                                    <td id="seat-36" data-name="36">36</td>
+                                    <td class="space">&nbsp;</td>
+                                    <td id="seat-37" data-name="37">37</td>
+                                    <td id="seat-38" data-name="38">38</td>
                                     </tr>
                                 </table>
                             </div>
