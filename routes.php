3695c407cce45ec2a410dd06464561eb8a81e9be
Ashwin <ashwinrockon123@gmail.com>
2021-06-15 02:38:17 +0530

    Routes are dynamically shown, Have to decide on what no. of seas to be shown

--- routes.php
+++ routes.php
@@ -11,20 +11,13 @@
         exit;
     }
 
-    // Operate on Search Route Form details
-    // echo "<pre>";
-    // var_export($_POST);
-    // echo "</pre>";
-    // die;
-
-    $source = $_POST["source"];
-    $destination = $_POST["destination"];
+    $source = strtoupper($_POST["source"]);
+    $destination = strtoupper($_POST["destination"]);
     $dep_date = $_POST["departure"];
 
-    // Search the tables if we have any routes thats matches the form details
     
-    
-    
+    $sql = "SELECT * FROM routes WHERE route_dep_date='$dep_date'";
+    $result = mysqli_query($conn, $sql);
 ?>
 
 
@@ -67,205 +60,89 @@
         <section id="searched-route">
             <ul>
                 <li class="searched-route-item" id="">Total Results : <span id="result-num">100</span></li>
-                <li class="searched-route-item">Trivandrum <span class="arrow">&rarr;</span> Ernakulam
-                <li class="searched-route-item" id="date">29/05/2021</li>
+                <li class="searched-route-item"><?php echo $source; ?> <span class="arrow">&rarr;</span> <?php echo $destination; ?>
+                <li class="searched-route-item" id="date"><?php echo $dep_date; ?></li>
             </ul>
         </section>
         <section id="searched-results">
-            <div id="searched-result-item">
-                <div id="route-id">
-                    <p>Route ID</p>
-                </div>
-                <div id="timing">
-                    <p>00:53 <span class="arrow">&rarr;</span>  06:23</p>
-                </div>
-                <div id="route-desc">
-                    <p id="main-route">
-                        <span id="source-route">Trivandrum</span> 
-                        <span class="arrow">&rarr;</span> 
-                        <span id="dest-route">
-                            Ernakulam
-                        </span>
-                    </p>
-                    <p id="cities">
-                        <span class="via">Via</span> KLM, ALP, EKM, TSR
-                     </p>
-                </div>
-                <div id="seats-desc">
-                    <div>
-                        <span id="num-seats">
-                        30
-                    </span> seats
+        <?php 
+            while($row = mysqli_fetch_assoc($result))
+            {
+                $citiesArr = explode(",",$row["route_cities"]);
+                
+                // Search the tables if we have any routes thats matches the form details
+                if(!in_array($source, $citiesArr) || !in_array($destination, $citiesArr) || !(array_search($source, $citiesArr) < array_search($destination, $citiesArr)))
+                    continue;
+                
+                $route_id = $row["route_id"];
+                $route_dep_time = $row["route_dep_time"];
+                $route_stepCost = $row["route_step_cost"];
+
+                $source_idx = array_search($source, $citiesArr);
+                $dest_idx = array_search($destination, $citiesArr);
+
+                $viaCities = implode(",", array_slice($citiesArr, $source_idx, $dest_idx - $source_idx));
+
+                $booking_amount = ($dest_idx - $source_idx) * $route_stepCost;
+            
+
+                ?>
+
+                <div id="searched-result-item">
+                    <div id="route-id">
+                        <p>
+                            <?php echo $route_id; ?>
+                        </p>
+                    </div>
+                    <div id="timing">
+                        <p>
+                            <?php 
+                                echo $route_dep_time;
+                            ?>
+                        </p>
+                    </div>
+                    <div id="route-desc">
+                        <p id="main-route">
+                            <span id="source-route">
+                                <?php 
+                                    echo $source;
+                                ?>
+                            </span> 
+                            <span class="arrow">&rarr;</span> 
+                            <span id="dest-route">
+                                <?php 
+                                    echo $destination;
+                                ?>
+                            </span>
+                        </p>
+                        <p id="cities">
+                            <span class="via">Via</span> 
+                            <?php 
+                                echo $viaCities;
+                            ?>
+                        </p>
+                    </div>
+                    <div id="seats-desc">
+                        <div>
+                            <span id="num-seats">
+                            <!-- Total or taken?? -->
+                            30
+                        </span> seats
+                        </div>
+                    </div>
+                    <div id="booking-desc">
+                        <p id="price"><i class="fas fa-rupee-sign"></i> 
+                            <?php 
+                                echo $booking_amount;
+                            ?></p>
+                        <button id="book-seat-btn">
+                            Select Seats
+                        </button>
                     </div>
                 </div>
-                <div id="booking-desc">
-                    <p id="price"><i class="fas fa-rupee-sign"></i> 100</p>
-                    <button id="book-seat-btn">
-                        Select Seats
-                    </button>
-                </div>
-            </div>
-            <div id="searched-result-item">
-                <div id="route-id">
-                    <p>Route ID</p>
-                </div>
-                <div id="timing">
-                    <p>00:53 <span class="arrow">&rarr;</span>  06:23</p>
-                </div>
-                <div id="route-desc">
-                    <p id="main-route">
-                        <span id="source-route">Trivandrum</span> 
-                        <span class="arrow">&rarr;</span> 
-                        <span id="dest-route">
-                            Ernakulam
-                        </span>
-                    </p>
-                    <p id="cities">
-                        <span class="via">Via</span> KLM, ALP, EKM, TSR
-                     </p>
-                </div>
-                <div id="seats-desc">
-                    <div>
-                        <span id="num-seats">
-                        30
-                    </span> seats
-                    </div>
-                </div>
-                <div id="booking-desc">
-                    <p id="price"><i class="fas fa-rupee-sign"></i> 100</p>
-                    <button id="book-seat-btn">
-                        Select Seats
-                    </button>
-                </div>
-            </div> <div id="searched-result-item">
-                <div id="route-id">
-                    <p>Route ID</p>
-                </div>
-                <div id="timing">
-                    <p>00:53 <span class="arrow">&rarr;</span>  06:23</p>
-                </div>
-                <div id="route-desc">
-                    <p id="main-route">
-                        <span id="source-route">Trivandrum</span> 
-                        <span class="arrow">&rarr;</span> 
-                        <span id="dest-route">
-                            Ernakulam
-                        </span>
-                    </p>
-                    <p id="cities">
-                        <span class="via">Via</span> KLM, ALP, EKM, TSR
-                     </p>
-                </div>
-                <div id="seats-desc">
-                    <div>
-                        <span id="num-seats">
-                        30
-                    </span> seats
-                    </div>
-                </div>
-                <div id="booking-desc">
-                    <p id="price"><i class="fas fa-rupee-sign"></i> 100</p>
-                    <button id="book-seat-btn">
-                        Select Seats
-                    </button>
-                </div>
-            </div> <div id="searched-result-item">
-                <div id="route-id">
-                    <p>Route ID</p>
-                </div>
-                <div id="timing">
-                    <p>00:53 <span class="arrow">&rarr;</span>  06:23</p>
-                </div>
-                <div id="route-desc">
-                    <p id="main-route">
-                        <span id="source-route">Trivandrum</span> 
-                        <span class="arrow">&rarr;</span> 
-                        <span id="dest-route">
-                            Ernakulam
-                        </span>
-                    </p>
-                    <p id="cities">
-                        <span class="via">Via</span> KLM, ALP, EKM, TSR
-                     </p>
-                </div>
-                <div id="seats-desc">
-                    <div>
-                        <span id="num-seats">
-                        30
-                    </span> seats
-                    </div>
-                </div>
-                <div id="booking-desc">
-                    <p id="price"><i class="fas fa-rupee-sign"></i> 100</p>
-                    <button id="book-seat-btn">
-                        Select Seats
-                    </button>
-                </div>
-            </div> <div id="searched-result-item">
-                <div id="route-id">
-                    <p>Route ID</p>
-                </div>
-                <div id="timing">
-                    <p>00:53 <span class="arrow">&rarr;</span>  06:23</p>
-                </div>
-                <div id="route-desc">
-                    <p id="main-route">
-                        <span id="source-route">Trivandrum</span> 
-                        <span class="arrow">&rarr;</span> 
-                        <span id="dest-route">
-                            Ernakulam
-                        </span>
-                    </p>
-                    <p id="cities">
-                        <span class="via">Via</span> KLM, ALP, EKM, TSR
-                     </p>
-                </div>
-                <div id="seats-desc">
-                    <div>
-                        <span id="num-seats">
-                        30
-                    </span> seats
-                    </div>
-                </div>
-                <div id="booking-desc">
-                    <p id="price"><i class="fas fa-rupee-sign"></i> 100</p>
-                    <button id="book-seat-btn">
-                        Select Seats
-                    </button>
-                </div>
-            </div> <div id="searched-result-item">
-                <div id="route-id">
-                    <p>Route ID</p>
-                </div>
-                <div id="timing">
-                    <p>00:53 <span class="arrow">&rarr;</span>  06:23</p>
-                </div>
-                <div id="route-desc">
-                    <p id="main-route">
-                        <span id="source-route">Trivandrum</span> 
-                        <span class="arrow">&rarr;</span> 
-                        <span id="dest-route">
-                            Ernakulam
-                        </span>
-                    </p>
-                    <p id="cities">
-                        <span class="via">Via</span> KLM, ALP, EKM, TSR
-                     </p>
-                </div>
-                <div id="seats-desc">
-                    <div>
-                        <span id="num-seats">
-                        30
-                    </span> seats
-                    </div>
-                </div>
-                <div id="booking-desc">
-                    <p id="price"><i class="fas fa-rupee-sign"></i> 100</p>
-                    <button id="book-seat-btn">
-                        Select Seats
-                    </button>
-                </div>
-            </div>
+           <?php  }
+        
+        ?>
         </section>
     </main>
 </body>
